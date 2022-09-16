bc62f19cb66615fc2d17626ef2156218428567fc
Emmanuel Olaniyan <rogyolani16@gmai.com>
2022-09-16 15:57:39 +0100

    added gitignor

---
+++ check_if_is_operator.py
@@ -0,0 +1,28 @@
+def check_operator(symbol, arr, should_sign_num, err):
+    if len(arr) == 1 and symbol in ('-', '+') and \
+            arr[0] in ('-', '+'):
+        arr[:] = [symbol]
+    elif len(arr) == 1 and symbol in ('/', '*') and \
+            arr[0] in ('-', '+'):
+        err()
+    elif len(arr) == 0 and symbol in ('-', '+'):
+        arr.append(symbol)
+    elif len(arr) == 0 and symbol in ('/', '*'):
+        err()
+    else:
+        if not should_sign_num:
+            if arr[len(arr) - 1] in ('-', '+', '*', '/'):
+                arr[len(arr) - 1] = symbol
+            elif arr[len(arr) - 1] not in ('-', '+', '*', '/'):
+                arr.append(symbol)
+        else:
+            if arr[len(arr) - 1] == '(':
+                if symbol in ('+', '-'):
+                    arr.append(symbol)
+                else:
+                    err()
+            elif symbol in ('-', '+'):
+                if arr[len(arr) - 1] in ('-', '+'):
+                    arr[len(arr) - 1] = symbol
+            elif symbol in ('/', '*'):
+                err()
